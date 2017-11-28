18fc6fd6d96ad68627d0bd43fba0db1fec40682c
Jaco Greeff <jacogr@gmail.com>
2017-11-28 12:37:59 +0100

    Jest package alias

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,1 +1,8 @@
-module.exports = require('@polkadot/dev/jest.config');
+const config = require('@polkadot/dev/jest.config');
+
+module.exports = Object.assign({}, config, {
+  moduleNameMapper: {
+    '@polkadot/client-p2p(.*)$': '<rootDir>/../client-p2p/src/$1',
+    '@polkadot/client-rpc(.*)$': '<rootDir>/../client-rpc/src/$1'
+  }
+});
