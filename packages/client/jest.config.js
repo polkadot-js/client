fd4d781281f18c01a12f5a961bc2352bf8a7a84e
Jaco Greeff <jacogr@gmail.com>
2017-11-28 16:13:12 +0100

    Cleanups, build from root

--- packages/client/jest.config.js
+++
@@ -1,8 +0,0 @@
-const config = require('@polkadot/dev/jest.config');
-
-module.exports = Object.assign({}, config, {
-  moduleNameMapper: {
-    '@polkadot/client-p2p(.*)$': '<rootDir>/../client-p2p/src/$1',
-    '@polkadot/client-rpc(.*)$': '<rootDir>/../client-rpc/src/$1'
-  }
-});
