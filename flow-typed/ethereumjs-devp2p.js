b563063119f387dce1425375d5a9f73d67d25c8a
Jaco Greeff <jacogr@gmail.com>
2017-11-29 17:58:07 +0100

    Move EthereumJS interface definitions

--- flow-typed/ethereumjs-devp2p.js
+++ flow-typed/ethereumjs-devp2p.js
@@ -10,6 +10,10 @@ declare module 'ethereumjs-devp2p' {
     tcpPort: number | null
   };
 
+  declare type EjsDevP2pDpt$Node = EjsDevP2pDpt$ConfigEndpoint & {
+    id: Buffer
+  };
+
   declare type EjsDevP2pDpt$Config = {
     clientId: string,
     endpoint: EjsDevP2pDpt$ConfigEndpoint
@@ -19,14 +23,25 @@ declare module 'ethereumjs-devp2p' {
     constructor (privateKey: Buffer, config: EjsDevP2pDpt$Config): EjsDevP2pDpt;
 
     bind (port: number, address: string): void;
+    bootstrap (node: EjsDevP2pDpt$ConfigEndpoint): Promise<EjsDevP2pDpt$Node>;
+    addPeer (node: EjsDevP2pDpt$ConfigEndpoint): Promise<EjsDevP2pDpt$Node>;
   }
 
-  declare class EjsDevP2pCapability extends EventEmitter {
-    constructor (version: string, peer: any, send: (code: number, payload: Buffer) => void): EjsDevP2pCapability;
+  declare type EjsDevP2pRlpx$Node = {
+    address: string,
+    id: Buffer,
+    port: number
+  };
+
+  declare interface EjsDevP2pCapability {
+    _handleMessage (code: number, data: Buffer): any;
+    getVersion (): number;
+    sendStatus (status: string): any;
+    sendMessage (code: number, payload: Array<Buffer>): any;
   }
 
   declare type EjsDevP2pRlpx$Capability = {
-    name: 'eth',
+    name: string,
     version: number,
     length: number,
     constructor: Class<EjsDevP2pCapability>
@@ -42,17 +57,26 @@ declare module 'ethereumjs-devp2p' {
   declare class EjsDevP2pRlpx extends EventEmitter {
     constructor (privateKey: Buffer, config: EjsDevP2pRlpx$Config): EjsDevP2pRlpx;
 
+    connect (node: EjsDevP2pRlpx$Node): Promise<any>;
     listen (port: number, address: string): void;
   }
 
-  declare class EjsDevP2pEth extends EjsDevP2pCapability {
+  declare class EjsDevP2pEth {
+    constructor (version: number, peer: EjsDevP2pRlpx$Node, send: (code: number, payload: Buffer) => void): EjsDevP2pEth;
+
     static eth62: EjsDevP2pRlpx$Capability;
     static eth63: EjsDevP2pRlpx$Capability;
+    static MESSAGE_CODES: {
+      [string]: number;
+    };
   }
 
   declare module.exports: {
     DPT: Class<EjsDevP2pDpt>,
     RLPx: Class<EjsDevP2pRlpx>,
-    ETH: Class<EjsDevP2pEth>
+    ETH: Class<EjsDevP2pEth>,
+    _util: {
+      int2buffer: (number) => Buffer
+    }
   }
 }
