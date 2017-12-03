497d6bf5deb9fe6244d6325d5cae00111740ca34
Jaco Greeff <jacogr@gmail.com>
2017-12-03 08:51:16 +0100

    Wasm assert

--- packages/client-wasm/src/create/instance.js
+++ packages/client-wasm/src/create/instance.js
@@ -1,18 +1,15 @@
 // ISC, Copyright 2017 Jaco Greeff
 // @flow
 
+const assert = require('@polkadot/util/assert');
 const isInstanceOf = require('@polkadot/util/is/instanceOf');
 
 const createImports = require('./imports');
 
-module.exports = function createInstance (module?: WebAssemblyModule, imports?: WebAssemblyImports): WebAssemblyInstance {
-  if (!module) {
-    throw new Error('WebAssembly module to be provided');
-  }
+module.exports = function createInstance (module: WebAssemblyModule, imports?: WebAssemblyImports): WebAssemblyInstance {
+  assert(module, 'WebAssembly module to be provided');
 
-  if (!isInstanceOf(module, WebAssembly.Module)) {
-    throw new Error('Cannot create from non-WebAssembly.Module');
-  }
+  assert(isInstanceOf(module, WebAssembly.Module), 'Cannot create from non-WebAssembly.Module');
 
   return new WebAssembly.Instance(module, createImports(imports));
 };
