3e17a2058738c0e841413c58c7a0efe75cc20d84
Jaco Greeff <jacogr@gmail.com>
2017-12-12 22:07:50 +0100

    Test short-circuit

--- packages/client-p2p/src/create/node.spec.js
+++ packages/client-p2p/src/create/node.spec.js
@@ -2,10 +2,25 @@
 
 const isInstanceOf = require('@polkadot/util/is/instanceOf');
 const LibP2P = require('libp2p');
+const PeerId = require('peer-id');
+const PeerInfo = require('peer-info');
 
 const createNode = require('./node');
 
 describe('createNode', () => {
+  let origPeerInfoCreate;
+
+  beforeEach(() => {
+    origPeerInfoCreate = PeerInfo.create;
+    PeerInfo.create = (callback) => {
+      origPeerInfoCreate(new PeerId(Buffer.from([])), callback);
+    };
+  });
+
+  afterEach(() => {
+    PeerInfo.create = origPeerInfoCreate;
+  });
+
   it('requires a valid config object', () => {
     return createNode().catch((error) => {
       expect(error.message).toMatch(/valid p2p config/);
@@ -19,7 +34,7 @@ describe('createNode', () => {
   });
 
   it('creates a valid LibP2p instance', async () => {
-    const libp2p = await createNode({ address: '127.0.0.1', port: 6789 }, { nodes: [] }, []);
+    const libp2p = await createNode({ address: '127.0.0.1', port: 6789 }, {});
 
     expect(
       isInstanceOf(libp2p, LibP2P)
