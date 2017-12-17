3db9f820d37a0be9806f5df82f20475c3c5d8d4e
Jaco Greeff <jacogr@gmail.com>
2017-12-17 21:42:30 +0100

    Split create functions for RPC

---
+++ packages/client-rpc/src/create/koa.js
@@ -0,0 +1,37 @@
+// ISC, Copyright 2017 Jaco Greeff
+// @flow
+
+import type { RpcType } from '../types';
+
+type CreateKoaOptionType = {
+  handlers: {
+    http: Function,
+    ws: Function
+  },
+  path: string,
+  types: Array<RpcType>
+};
+
+const Koa = require('koa');
+const koaRoute = require('koa-route');
+const koaWebsocket = require('koa-websocket');
+
+module.exports = function createKoa ({ handlers, path, types }: CreateKoaOptionType): Koa {
+  let app = new Koa();
+
+  if (types.includes('http')) {
+    app.use(
+      koaRoute.post(path, handlers.http)
+    );
+  }
+
+  if (types.includes('ws')) {
+    app = koaWebsocket(app);
+
+    (app: any).ws.use(
+      koaRoute.all(path, handlers.ws)
+    );
+  }
+
+  return app;
+};
