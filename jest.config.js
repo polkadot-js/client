f7284de080344a9de3af25a91f1a6e90ec31097e
Jaco Greeff <jacogr@gmail.com>
2017-12-17 20:38:13 +0100

    Update flow/jest references for DB

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,10 @@ const config = require('@polkadot/dev/config/jest');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
+    '@polkadot/client-chains(.*)$': '<rootDir>/packages/client-chains/src/$1',
+    '@polkadot/client-db(.*)$': '<rootDir>/packages/client-db/src/$1',
     '@polkadot/client-p2p(.*)$': '<rootDir>/packages/client-p2p/src/$1',
     '@polkadot/client-rpc(.*)$': '<rootDir>/packages/client-rpc/src/$1',
-    '@polkadot/client-types(.*)$': '<rootDir>/packages/client-types/src/$1',
     '@polkadot/client-wasm(.*)$': '<rootDir>/packages/client-wasm/src/$1'
   }
 });
