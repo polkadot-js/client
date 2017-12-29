c9ec80b748c242697f430981a7ae7ecbe1ea43bc
Jaco Greeff <jacogr@gmail.com>
2017-12-29 22:45:24 +0100

    RuntimeInterface type

--- packages/client-wasm-runtime/src/index.js
+++ packages/client-wasm-runtime/src/index.js
@@ -1,12 +1,16 @@
 // ISC, Copyright 2017 Jaco Greeff
 // @flow
 
+import type { RuntimeInterface } from './types';
+
 const memory = require('./memory');
 const runtime = require('./runtime');
 
 module.exports = function (wasmMemory: WebAssembly.Memory): any {
   const rt = runtime(wasmMemory);
-  const exports = Object.assign({}, memory(rt));
+  const exports: RuntimeInterface = Object.assign(
+    ({}: $Shape<RuntimeInterface>), memory(rt)
+  );
 
   return Object
     .keys(exports)
--- packages/client-wasm-runtime/src/types.js
+++ packages/client-wasm-runtime/src/types.js
@@ -18,3 +18,15 @@ export type RuntimeType = {
 };
 
 export type PointerType = number;
+
+export type RuntimeInterface$Memory = {
+  free: (ptr: PointerType) => void,
+  malloc: (size: number) => PointerType,
+  memcpy: (dst: PointerType, src: PointerType, num: number) => PointerType,
+  memmove: (dst: PointerType, src: PointerType, num: number) => PointerType,
+  memset: (dst: PointerType, val: number, num: number) => PointerType
+};
+
+export type RuntimeInterface = {
+  memory: RuntimeInterface$Memory
+};
