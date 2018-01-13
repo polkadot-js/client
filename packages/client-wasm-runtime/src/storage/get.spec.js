af08861cf5a7c10f8f4a3d2abccec59c2624c7cb
Jaco Greeff <jacogr@gmail.com>
2018-01-13 12:39:03 +0100

    Storage get (sync)

---
+++ packages/client-wasm-runtime/src/storage/get.spec.js
@@ -0,0 +1,52 @@
+/* eslint camelcase: 0 */
+
+const index = require('./index');
+
+describe('get_storage_into', () => {
+  let heap;
+  let storage;
+  let get_storage_into;
+
+  beforeEach(() => {
+    const uint8 = new Uint8Array([0x53, 0x61, 0x79, 0x48, 0x65, 0x6c, 0x6c, 0x6f]);
+
+    heap = {
+      get: (ptr, len) => uint8.subarray(ptr, ptr + len),
+      set: jest.fn()
+    };
+
+    storage = {
+      get: jest.fn((key) => Promise.resolve(
+        new Uint8Array([0x1, 0x2, 0x3, 0x4, 0x5]))
+      )
+    };
+
+    get_storage_into = index({ heap, storage }).get_storage_into;
+  });
+
+  it('retrieves the correct value from storage', () => {
+    get_storage_into(0, 3, 3, 3);
+
+    expect(
+      storage.get
+    ).toHaveBeenCalledWith('Say');
+  });
+
+  it('retrieves the full value when length >= available', () => {
+    expect(
+      get_storage_into(0, 0, 3, 10)
+    ).toEqual(5);
+    expect(
+      heap.set
+    ).toHaveBeenCalledWith(3, new Uint8Array([0x1, 0x2, 0x3, 0x4, 0x5]));
+  });
+
+  it('retrieves a partial value when length < available', () => {
+    expect(
+      get_storage_into(0, 0, 3, 3)
+    ).toEqual(3);
+    expect(
+      heap.set
+    ).toHaveBeenCalledWith(3, new Uint8Array([0x1, 0x2, 0x3]));
+  });
+});
--- packages/client-wasm-runtime/src/storage/index.js
+++ packages/client-wasm-runtime/src/storage/index.js
@@ -3,10 +3,18 @@
 
 import type { RuntimeEnv, RuntimeInterface$Storage, PointerType } from '../types';
 
+const get = require('./get');
 const set = require('./set');
 
 module.exports = function storage ({ heap, storage }: RuntimeEnv): RuntimeInterface$Storage {
   return {
+    get_storage_into: (keyPtr: PointerType, keyLength: number, dataPtr: PointerType, dataLength: number): number => {
+      const data = get(storage, heap.get(keyPtr, keyLength), dataLength);
+
+      heap.set(dataPtr, data);
+
+      return data.length;
+    },
     set_storage: (keyPtr: PointerType, keyLength: number, dataPtr: PointerType, dataLength: number): void =>
       set(storage, heap.get(keyPtr, keyLength), heap.get(dataPtr, dataLength))
   };
