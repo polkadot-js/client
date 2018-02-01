dda214e0ec7e96bf0760d896e0ca087a0ed1a509
Jaco Greeff <jacogr@gmail.com>
2018-02-01 12:31:52 +0100

    Align stacksize with Rust executor

--- packages/client-wasm-runtime/src/env.js
+++ packages/client-wasm-runtime/src/env.js
@@ -8,8 +8,7 @@ import type { RuntimeEnv, PointerType } from './types';
 
 const l = require('@polkadot/util/logger')('runtime');
 
-// 2 ^ 14 https://github.com/jfbastien/musl/blob/190dffd1415cc8be52d4659aced51625d63bdbc1/arch/wasm32/wasm.js#L403
-const START_OFFSET = 16384;
+const STACK_SIZE = 32768;
 
 module.exports = function createEnv ({ buffer }: WebAssembly.Memory, storage: DbInterface): RuntimeEnv {
   const uint8 = new Uint8Array(buffer);
@@ -21,7 +20,7 @@ module.exports = function createEnv ({ buffer }: WebAssembly.Memory, storage: Db
       uint8,
       alloc: {},
       freed: {},
-      offset: START_OFFSET,
+      offset: STACK_SIZE,
       size: buffer.byteLength,
       dup: (ptr: PointerType, len: number): Uint8Array => uint8.slice(ptr, ptr + len),
       get: (ptr: PointerType, len: number): Uint8Array => uint8.subarray(ptr, ptr + len),
