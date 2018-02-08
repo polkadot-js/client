3673e420648b18be48a404013a298606ad8240ca
Jaco Greeff <jacogr@gmail.com>
2018-02-08 16:36:29 +0100

    Case

--- packages/client-wasm-runtime/src/crypto/twox128.js
+++ packages/client-wasm-runtime/src/crypto/twox128.js
@@ -3,8 +3,8 @@
 // of the ISC license. See the LICENSE file for details.
 // @flow
 
-const xxhashAsU8A128 = require('@polkadot/util-crypto/xxhash/asU8A128');
+const xxhashAsU8a128 = require('@polkadot/util-crypto/xxhash/asU8a128');
 
 module.exports = function twox128 (data: Uint8Array): Uint8Array {
-  return xxhashAsU8A128(data);
+  return xxhashAsU8a128(data);
 };
