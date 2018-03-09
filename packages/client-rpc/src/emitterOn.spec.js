e84f80a6c615bc3d983f6b1034beee2301074eb7
Jaco Greeff <jacogr@gmail.com>
2018-03-09 11:02:00 +0100

    Simple emitter tests

---
+++ packages/client-rpc/src/emitterOn.spec.js
@@ -0,0 +1,25 @@
+// Copyright 2017-2018 Jaco Greeff
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+const EventEmitter = require('eventemitter3');
+
+const emitterOn = require('./emitterOn');
+
+describe('emitterOn', () => {
+  let self;
+
+  beforeEach(() => {
+    self = {
+      emitter: new EventEmitter()
+    };
+  });
+
+  it('wraps event emitter handlers', (done) => {
+    emitterOn(self)('event', () => {
+      done();
+    });
+
+    self.emitter.emit('event');
+  });
+});
