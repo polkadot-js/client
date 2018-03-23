5330c80d4260500347e66868581648730e3dccb8
Jaco Greeff <jacogr@gmail.com>
2018-03-23 14:52:58 +0100

    Cleanup consensys

--- packages/client-db-chain/src/state/consensys/index.js
+++ packages/client-db-chain/src/state/consensys/index.js
@@ -3,17 +3,26 @@
 // of the ISC license. See the LICENSE file for details.
 // @flow
 
+import type BN from 'bn.js';
 import type { WrapDbInterface } from '@polkadot/client-db/types';
 import type { ChainDb$State$Consensys } from './types';
 
-const { kNv32 } = require('../../getset');
-const authority = require('./authority');
+const { k32vU, kNv32 } = require('../../getset');
 
 const keys = require('./keys');
 
 module.exports = function consensys (db: WrapDbInterface): ChainDb$State$Consensys {
+  const authorityH = k32vU(db, keys.AUTHORITY_H);
+  const authorityU = k32vU(db, keys.AUTHORITY_U);
+
   return {
-    authority: authority(db),
+    authority: {
+      get: authorityH.get,
+      set: (id: BN | number, publicKey: Uint8Array): void => {
+        authorityH.set(id, publicKey);
+        authorityU.set(id, publicKey);
+      }
+    },
     authorityCount: kNv32(db, keys.AUTHORITY_LENGTH)
   };
 };
