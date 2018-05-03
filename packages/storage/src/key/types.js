5ceb30134d0c081f75afea6fe3409db0c869065d
Jaco Greeff <jacogr@gmail.com>
2018-05-03 14:40:41 +0200

    Move storage to types repo

--- packages/storage/src/key/types.js
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2018 Jaco Greeff
-// This software may be modified and distributed under the terms
-// of the ISC license. See the LICENSE file for details.
-// @flow
-
-import type { StorageDef$Key$Values } from '../types';
-
-export type Keygen = (...keyParams?: StorageDef$Key$Values) => Uint8Array;
