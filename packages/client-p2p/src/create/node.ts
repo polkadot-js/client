b331b786084ca86f5fb0eeba0fa2aa9f6b9d99c1
Jaco Greeff <jacogr@gmail.com>
2018-07-02 10:57:03 +0200

    Update to libp2p 0.22.0

--- packages/client-p2p/src/create/node.ts
+++ packages/client-p2p/src/create/node.ts
@@ -10,21 +10,28 @@ import createModules from './modules';
 import createListener from './listener';
 import createPeerBook from './peerBook';
 
+const config = {
+  dht: {
+    kBucketSize: 20
+  },
+  EXPERIMENTAL: {
+    pubsub: false,
+    dht: true
+  }
+};
+
 export default async function createNode ({ config: { p2p: { address, port, nodes = [] } }, l }: P2pState): Promise<Libp2p> {
   const peerBook = await createPeerBook([]);
-  const listener = await createListener(address, port);
-  const modules = createModules(listener, nodes);
-  const addrs = listener.multiaddrs.toArray().map((addr) => addr.toString());
+  const peerInfo = await createListener(address, port);
+  const modules = createModules(peerInfo, nodes);
+  const addrs = peerInfo.multiaddrs.toArray().map((addr) => addr.toString());
 
   l.log(`creating Libp2p with ${addrs.join(', ')}`);
 
-  const node = new Libp2p(modules, listener, peerBook);
-
-  // HACK stub floodsub (it is not enabled for Polkadot Rust, however it is a default for JS, noop the start). With libp2p 0.21.0 this won't be needed anymore since floodsub can be switched on/off
-  // @ts-ignore
-  node._floodSub.start = (cb) => {
-    cb();
-  };
-
-  return node;
+  return new Libp2p({
+    config,
+    modules,
+    peerBook,
+    peerInfo
+  });
 }
