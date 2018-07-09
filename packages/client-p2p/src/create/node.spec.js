f639a0dd76d371e8717e9ecf0a582871d316899a
Jaco Greeff <jacogr@gmail.com>
2018-07-09 18:00:13 +0200

    Update (now) failing test

--- packages/client-p2p/src/create/node.spec.js
+++ packages/client-p2p/src/create/node.spec.js
@@ -11,6 +11,8 @@ import logger from '@polkadot/util/logger';
 
 import createNode from './node';
 
+const l = logger('test');
+
 describe('createNode', () => {
   let origPeerInfoCreate;
   let count = 0;
@@ -27,7 +29,7 @@ describe('createNode', () => {
   });
 
   it('creates a valid LibP2p instance', async () => {
-    const libp2p = await createNode({ p2p: { address: '127.0.0.1', port: 36789 } });
+    const libp2p = await createNode({ p2p: { address: '127.0.0.1', port: 36789 } }, l);
 
     expect(
       isInstanceOf(libp2p, LibP2P)
