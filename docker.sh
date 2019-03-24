748cbd9c555db4abd35942b82de100c34c08d5e3
Jaco Greeff <jacogr@gmail.com>
2019-03-24 21:55:43 +0100

    Add version to docker image via docker.sh (#210)

--- docker.sh
+++ docker.sh
@@ -3,30 +3,47 @@
 set -e
 
 CMD="$1"
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | sed 's/ //g')
+
+function build () {
+  echo "*** Creating Dokcerfile from latest version (via template)"
+  sed "s/VERSION/$VERSION/g" Dockerfile.tmpl > Dockerfile
+
+  echo "*** Building polkadot-js-client"
+  docker build -t polkadot-js-client .
+
+  exit 0
+}
+
+function publish () {
+  echo "*** Tagging polkadot-js-client $VERSION"
+  docker tag polkadot-js-client jacogr/polkadot-js-client:$VERSION
+
+  echo "*** Publishing polkadot-js-client $VERSION"
+  docker push jacogr/polkadot-js-client:$VERSION
+
+  exit 0
+}
+
+function usage () {
+  echo "Usage: docker.sh <build|publish>"
+
+  exit 1
+}
 
 case $CMD in
   build)
-    echo "*** Building polkadot-js-client"
-    docker build -t polkadot-js-client .
-    exit 0
+    build
     ;;
   publish)
-    VERSION="$2"
-
-    if [ -z "$VERSION" ]; then
-      echo "Error: Expected <version> after publish"
-      exit 1
-    fi
-
-    echo "*** Tagging polkadot-js-client $VERSION"
-    docker tag polkadot-js-client jacogr/polkadot-js-client:$VERSION
-
-    echo "*** Publishing polkadot-js-client $VERSION"
-    docker push jacogr/polkadot-js-client:$VERSION
-    exit 0
+    publish
     ;;
   *)
-    echo "Usage: docker.sh <build|publish>"
-    exit 1
+    usage
     ;;
 esac
